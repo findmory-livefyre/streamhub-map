29cb5a6718c08775ed849541a8c28335b9908309
Mike Ory <findmory@gmail.com>
2014-01-08 15:34:53 -0500

    changed collection.  lowered amount of 'more' presses

--- js/map.js
+++ js/map.js
@@ -28,9 +28,9 @@ collection.pipe(view);
 //preferred format
 Livefyre.require(['streamhub-sdk/collection','streamhub-map/content/content-map-view'], function(Collection,ContentMapView){
     var collection = new Collection({
-        network: "client-solutions-turner.fyre.co",
-        siteId: 343292,
-        articleId: "custom-1387221206185"
+        network: "client-solutions.fyre.co",
+        siteId: 333682,
+        articleId: "sn-1389203417469"
     });
     var view = new ContentMapView({
         el: document.getElementById("lf_map"),
